eaffd3f06db2d85fa673e7665635136089bc8763
jessicagrayson <grayjessica12@hotmail.com>
2023-10-04 18:55:29 +0000

    added GET endpoint to server.js - can now read entry by its id

--- server/server.js
+++ server/server.js
@@ -82,6 +82,32 @@ app.post('/api/entryform', async (req, res) => {
   }
 });
 
+// GETS entry values
+app.get('/api/entries/:entryId', async (req, res) => {
+  try {
+    const entryId = Number(req.params.entryId);
+    if (!Number.isInteger(entryId) || entryId <= 0) {
+      throw new ClientError(400, '"entryId" must be an integer');
+    }
+    const sql = `
+    SELECT * from "entries"
+    WHERE "entryId" = $1
+    `;
+
+    const params = [entryId];
+    const result = await db.query(sql, params);
+    const entry = result.rows[0];
+
+    if (!entry) {
+      throw new ClientError(404, `Cannot find entry with ID ${entryId}`);
+    }
+
+    res.status(200).json(entry);
+  } catch (error) {
+    console.error(error);
+  }
+});
+
 /**
  * Serves React's index.html if no api route matches.
  *
