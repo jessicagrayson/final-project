5bb15db2630e784ac61dcad970f9c46e01243531
jessicagrayson <grayjessica12@hotmail.com>
2023-10-08 06:53:23 +0000

    implemented basic routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,39 +1,22 @@
-import { useEffect, useState } from 'react';
+// import { useEffect, useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
 import Form from './Form.jsx';
 import EntryForm from './EntryForm.jsx';
 // import Entry from './Entry.jsx';
-import EntryView from './EntryView.jsx';
+// import EntryView from './EntryView.jsx';
 import EntriesList from './EntriesList.jsx';
-import GophrLogo from './GophrLogo.jsx';
+// import GophrLogo from './GophrLogo.jsx';
 import './App.css';
 
 export default function App() {
-  const [serverData, setServerData] = useState('');
-
-  useEffect(() => {
-    async function readServerData() {
-      const resp = await fetch('/api/hello');
-      const data = await resp.json();
-
-      console.log('Data from server:', data);
-
-      setServerData(data.message);
-    }
-
-    readServerData();
-  }, []);
-
   return (
-    <>
-      <div>
-        <GophrLogo />
-        <Form />
-        <EntryForm />
-        {/* <Entry /> */}
-        <EntryView />
-        <EntriesList />
-      </div>
-      <h1>{serverData}</h1>
-    </>
+    <div>
+      <Routes>
+        <Route path="/" element={<Form />}>
+          <Route index element={<EntriesList />} />
+          <Route path="register" element={<EntryForm />} />
+        </Route>
+      </Routes>
+    </div>
   );
 }
