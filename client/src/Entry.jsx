0cd970a7d6c37fc6deaa4357de20c424c5b01402
jessicagrayson <grayjessica12@hotmail.com>
2023-10-12 18:11:41 +0000

    styles applied to EntriesList.jsx

--- client/src/Entry.jsx
+++ client/src/Entry.jsx
@@ -2,7 +2,7 @@ import React, { useState, useCallback } from 'react';
 import ImageField from './ImageField';
 import LinkComponent from './LinkComponent';
 import Input from './Input';
-// import BlurbComponent from './BlurbComponent';
+import BlurbComponent from './BlurbComponent';
 
 export default function Entry({ entry }) {
   const [expanded, setExpanded] = useState(false);
@@ -22,42 +22,40 @@ export default function Entry({ entry }) {
   }
 
   return (
-    <div className="flex">
-      <form>
-        <ImageField
-          src={entry.imageUrl}
-          placeholder="Image URL"
-          value={entry.imageUrl}
-          width="200"
-          onChange={(e) => entry.onChange('Image Url', e.target.value)}
-        />
-        <div>
+    <div className="flex space-x-4">
+      <ImageField src={entry.imageUrl} alt="Image" className="w-40 h-40 my-1" />
+      <div className="flex flex-col">
+        <div className="flex flex-col items-start">
           <Input
-            className="text-indigo-400"
+            className="text-sm font-semibold"
             placeholder="Location"
             value={entry.location}
             onChange={(e) => entry.onChange('location', e.target.value)}
           />
           <Input
-            className="text-indigo-400"
+            className="text-sm font-medium"
             placeholder="Travel Date"
             value={formatISODate(entry.travelDate)}
             onChange={(e) => entry.onChange('Travel Date', e.target.value)}
           />
         </div>
-        {/* <BlurbComponent
-          className="text-black"
-          placeholder="Blurb"
-          value={entry.blurb}
-          onChange={(e) => entry.onChange('Blurb', e.target.value)}
-        /> */}
+        {expanded && (
+          <BlurbComponent
+            className="text-black"
+            placeholder="Blurb"
+            value={entry.blurb}
+            onChange={(e) => entry.onChange('Blurb', e.target.value)}
+          />
+        )}
+      </div>
+      <div className="flex items-end">
         <LinkComponent
           to={`/entries/${entry.entryId}`}
           onClick={handleExpandClick}
-          placeholder={expanded ? 'Collapse' : 'Expand'}
-          className="text-indigo-500"
+          placeholder={expanded ? 'Collapse' : 'Expand Entry'}
+          className="text-xs text-indigo-400 hover:underline hover:text-indigo-600"
         />
-      </form>
+      </div>
     </div>
   );
 }
--- client/src/Scratch.jsx
+++ client/src/Scratch.jsx
@@ -1,90 +1,39 @@
-// import React, { useState } from 'react';
-// import { useNavigate } from 'react-router-dom';
-// import Input from './Input';
-// import CustomButton from './CustomButton';
-// import LinkComponent from './LinkComponent';
-
-// export default function SignUp() {
-//   const navigate = useNavigate();
-//   const [username, setUsername] = useState('');
-//   const [password, setPassword] = useState('');
-
-//   const handleUsernameChange = (e) => {
-//     setUsername(e.target.value);
-//   };
-
-//   const handlePasswordChange = (e) => {
-//     setPassword(e.target.value);
-//   };
-
-//   const handleSubmit = async (e) => {
-//     e.preventDefault();
-//     // Creates new user object from values
-//     const newUser = {
-//       username: username,
-//       password: password,
-//     };
-
-//     try {
-//       const res = await fetch('/api/register', {
-//         method: 'POST',
-//         headers: {
-//           'Content-type': 'application/json',
-//         },
-//         body: JSON.stringify(newUser),
-//       });
-
-//       if (!res.ok) {
-//         throw new Error(`Fetch error ${res.status}`);
-//       }
-//       const user = await res.json();
-//       console.log('Registered:', user);
-//       navigate('/list');
-//     } catch (error) {
-//       alert(`Error registering user: ${error}`);
-//     }
-//     // Logs new values
-//     console.log('username:', username);
-//     console.log('password', password);
-//     console.log('newUser', newUser);
-//   };
-
-//   return (
-//     <div>
-//       <h3>Welcome! Create an account</h3>
-//       <form onSubmit={handleSubmit} className="flex flex-col">
-//         <label htmlFor="username" className="text-indigo-600">
-//           Username:
-//         </label>
-//         <Input
-//           id="username"
-//           name="username"
-//           onChange={handleUsernameChange}
-//           type={'text'}
-//           placeholder={''}
-//           autoComplete="username"
-//           className="mt-5 ml-10 border-2 border-indigo-400 rounded-sm bg-zinc-200 w-80 h-9"
-//         />
-//         <label htmlFor="password" className="text-indigo-600">
-//           Password:
-//         </label>
-
-//         <Input
-//           id="password"
-//           name="password"
-//           onChange={handlePasswordChange}
-//           type={'password'}
-//           placeholder={''}
-//           autoComplete="current-password"
-//           className="mt-5 ml-10 border-2 border-indigo-400 rounded-sm bg-zinc-200 w-80 h-9"
-//         />
-//         <CustomButton type="submit" label="Sign Up" />
-//       </form>
-//       <LinkComponent
-//         to="/list"
-//         className="text-indigo-600"
-//         placeholder="See all entries"
-//       />
-//     </div>
-//   );
-// }
+{
+  /* <div className="flex">
+  <form>
+    <ImageField
+      src={entry.imageUrl}
+      placeholder="Image URL"
+      value={entry.imageUrl}
+      width="200"
+      onChange={(e) => entry.onChange('Image Url', e.target.value)}
+    />
+    <div>
+      <Input
+        className="text-indigo-400"
+        placeholder="Location"
+        value={entry.location}
+        onChange={(e) => entry.onChange('location', e.target.value)}
+      />
+      <Input
+        className="text-indigo-400"
+        placeholder="Travel Date"
+        value={formatISODate(entry.travelDate)}
+        onChange={(e) => entry.onChange('Travel Date', e.target.value)}
+      />
+    </div>
+    <BlurbComponent
+          className="text-black"
+          placeholder="Blurb"
+          value={entry.blurb}
+          onChange={(e) => entry.onChange('Blurb', e.target.value)}
+        />
+    <LinkComponent
+      to={`/entries/${entry.entryId}`}
+      onClick={handleExpandClick}
+      placeholder={expanded ? 'Collapse' : 'Expand'}
+      className="text-indigo-500"
+    />
+  </form>
+</div>; */
+}
