f80441e340e196c6fec1c9e569f76e9e2c999e9c
jessicagrayson <grayjessica12@hotmail.com>
2023-10-20 05:49:15 +0000

    EntryView is now responsive and mobile-friendly

--- client/src/EntryView.jsx
+++ client/src/EntryView.jsx
@@ -52,18 +52,21 @@ export default function EntryView() {
         className="text-xl text-indigo-500 hover:underline"
       />
       <div className="flex items-center justify-center">
-        <div className="flex flex-col items-center justify-center w-3/4 px-12 py-8">
+        <div className="flex flex-col-reverse items-center justify-center px-12 py-8 lg:w-3/4 md:w-3/4">
           <div className="w-full">
-            <p className="text-xl font-medium">
+            {/* <p className="text-xl font-medium">
               {entry.location} | {formatISODate(entry.travelDate)}
-            </p>
+            </p> */}
           </div>
-          <div className="flex ">
-            <div className="w-2/3">
-              <img src={entry.imageUrl} className="rounded-sm" />
+          <div className="flex flex-col lg:flex-row md:flex-row">
+            <div className="w-full lg:w-2/3 md:2/3">
+              <img src={entry.imageUrl} className="rounded-md" />
             </div>
-            <div className="flex flex-col w-1/3 p-2 ml-4 rounded-sm bg-slate-100">
-              <ViewMenu />
+            <div className="flex flex-col w-full p-2 rounded-sm lg:ml-4 md:ml-4 lg:w-1/3 md:w-1/3 bg-slate-100">
+              <p className="font-medium text-md">
+                {entry.location} | {formatISODate(entry.travelDate)}
+                <ViewMenu />
+              </p>
               <p className="leading-normal">{entry.blurb}</p>
             </div>
           </div>
