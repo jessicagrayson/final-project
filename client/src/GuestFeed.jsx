994288ca1864dfbba010a9c2cb4965712a9acc15
jessicagrayson <grayjessica12@hotmail.com>
2023-11-22 22:21:40 +0000

    guest-feed route defined

---
+++ client/src/GuestFeed.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import GophrLogo from './GophrLogo';
+// import FadeMenu from './MenuCustomList';
+import NavBar from './NavBar';
+// import { useLocation } from 'react-router-dom';
+
+export default function GuestFeed() {
+  return (
+    <div>
+      <NavBar />
+      <GophrLogo />
+      <p>Hello world</p>
+    </div>
+  );
+}
