acd742c809e1a84602901d96ef8bd1c8d7a6c26b
jessicagrayson <grayjessica12@hotmail.com>
2023-11-23 06:30:09 +0000

    storing changes

--- client/src/GuestFeed.jsx
+++ client/src/GuestFeed.jsx
@@ -3,9 +3,5 @@ import React from 'react';
 // import { useLocation } from 'react-router-dom';
 
 export default function GuestFeed() {
-  return (
-    <div>
-      <p>Hello world</p>
-    </div>
-  );
+  return <div></div>;
 }
