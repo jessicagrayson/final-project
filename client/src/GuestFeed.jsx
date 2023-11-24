9252d246f35b2dedf9ef78a9fb8b998a1a3a464d
jessicagrayson <grayjessica12@hotmail.com>
2023-11-24 21:35:12 +0000

    dummy data for demo created

--- client/src/GuestFeed.jsx
+++ client/src/GuestFeed.jsx
@@ -1,7 +1,42 @@
 import React from 'react';
-// import FadeMenu from './MenuCustomList';
-// import { useLocation } from 'react-router-dom';
+
+const demoData = [
+  {
+    id: 1,
+    location: 'Irvine, CA',
+    // travelDate: '11/23/2023',
+    // add imageUrl before blurb
+    blurb: 'This is a test entry',
+  },
+  {
+    id: 2,
+    location: 'Chino, CA',
+    // travelDate: '11/19/2023',
+    // add imageUrl before blurb
+    blurb: 'This is the second test entry',
+  },
+  {
+    id: 3,
+    location: 'Portland, OR',
+    // travelDate: '4/14/2023',
+    // add imageUrl before blurb
+    blurb: 'This is the third test entry',
+  },
+];
 
 export default function GuestFeed() {
-  return <div></div>;
+  return (
+    <div>
+      <div className="flex flex-row flex-wrap items-center">
+        {demoData.map((entry) => (
+          <div key={entry.id} className="w-1/4 p-4">
+            {/* Add your styling and content based on your needs */}
+            <h2 className="mb-2 text-lg font-bold">{entry.location}</h2>
+            {/* Add other fields like travelDate or imageUrl here */}
+            <p className="text-sm">{entry.blurb}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
