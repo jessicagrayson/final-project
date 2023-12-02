97bc507ade7612796a00e16ce33c5bbbec1d3c0c
jessicagrayson <grayjessica12@hotmail.com>
2023-12-02 08:39:35 +0000

    guest feed grid layout complete

--- client/src/GuestFeed.jsx
+++ client/src/GuestFeed.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Input from './Input';
 import ImageField from './ImageField';
+import LinkComponent from './LinkComponent';
 
 const demoData = [
   {
@@ -8,21 +9,56 @@ const demoData = [
     location: 'Irvine, CA',
     travelDate: 'June 7, 2023',
     imageUrl: 'public/images/grandcanyon.JPG',
-    blurb: 'This is a test entry',
+    // blurb: 'This is a test entry',
   },
   {
     id: 2,
     location: 'Big Sur, CA',
     travelDate: 'November 19, 2023',
     imageUrl: 'public/images/BigSur.JPG',
-    blurb: 'This is the second test entry',
+    // blurb: 'This is the second test entry',
   },
   {
     id: 3,
     location: 'Portland, OR',
     travelDate: 'April 14, 2023',
     imageUrl: 'public/images/Mt. Hood.jpeg',
-    blurb: 'This is the third test entry',
+    // blurb: 'This is the third test entry',
+  },
+  {
+    id: 4,
+    location: 'Irvine, CA',
+    travelDate: 'June 7, 2023',
+    imageUrl: 'public/images/grandcanyon.JPG',
+    // blurb: 'This is a test entry',
+  },
+  {
+    id: 5,
+    location: 'Big Sur, CA',
+    travelDate: 'November 19, 2023',
+    imageUrl: 'public/images/BigSur.JPG',
+    // blurb: 'This is the second test entry',
+  },
+  {
+    id: 6,
+    location: 'Portland, OR',
+    travelDate: 'April 14, 2023',
+    imageUrl: 'public/images/Mt. Hood.jpeg',
+    // blurb: 'This is the third test entry',
+  },
+  {
+    id: 7,
+    location: 'Big Sur, CA',
+    travelDate: 'November 19, 2023',
+    imageUrl: 'public/images/BigSur.JPG',
+    // blurb: 'This is the second test entry',
+  },
+  {
+    id: 8,
+    location: 'Portland, OR',
+    travelDate: 'April 14, 2023',
+    imageUrl: 'public/images/Mt. Hood.jpeg',
+    // blurb: 'This is the third test entry',
   },
 ];
 
@@ -30,14 +66,12 @@ export default function GuestFeed() {
   return (
     <div className="flex flex-row flex-wrap items-center justify-center gap-y-2 gap-x-3">
       {demoData.map((entry) => (
-        // BG color of each thing should be slate-200
         <div
           key={entry.id}
-          // CSS below is duplicate, needs to be removed
           className="flex p-3 mb-2 space-x-4 rounded-md bg-slate-200 hover:-translate-y-0.5 transition ease-in-out delay-70">
           <ImageField
             src={entry.imageUrl}
-            className="w-40 h-40 my-1 rounded-md g-40"
+            className="w-40 h-40 my-1 rounded-md"
           />
 
           <div className="flex flex-col">
@@ -52,11 +86,12 @@ export default function GuestFeed() {
               value={entry.travelDate}
             />
           </div>
-          {/* <Input
-                className="text-sm font-semibold bg-red-200"
-                placeholder="Location"
-                value={entry.blurb}
-              /> */}
+          <div className="flex items-end">
+            <LinkComponent
+              placeholder={'Details'}
+              className="text-sm text-indigo-500 hover:underline hover:text-indigo-600"
+            />
+          </div>
         </div>
       ))}
     </div>
