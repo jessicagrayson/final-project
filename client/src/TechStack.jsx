5efb0d649764115a08ae692dd8d0e0f53ef9eaf0
jessicagrayson <grayjessica12@hotmail.com>
2023-12-05 07:35:57 +0000

    drafted list of technologies and resources

--- client/src/TechStack.jsx
+++ client/src/TechStack.jsx
@@ -13,26 +13,37 @@ export default function TechStack() {
     'AWS',
     'React Router',
     'Multer',
-    'Figma',
-    'DBDiagram',
-    'Vite',
     'SQL',
     'Argon',
     'JWT',
     'Elastic Beanstalk',
+  ];
+
+  const resources = [
+    'Figma',
+    'DBDiagram',
+    'Vite',
     'Visual Studio Code',
     'Github',
   ];
 
   return (
-    <div>
+    <div className="flex flex-col items-center w-2/3 p-4 m-6 space-y-3 text-lg rounded-sm bg-slate-100">
       <p>
         Gophr is a full-stack photo blogging web application built by Jessica
         Grayson with a little help from the following resources and
         technologies:
       </p>
-      <ul>
+      <ul className="list-disc">
+        <h1 className="font-semibold">Tech Stack:</h1>
         {technologies.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+      <ul className="list-disc">
+        <h1 className="font-semibold">Resources:</h1>
+
+        {resources.map((item, index) => (
           <li key={index}>{item}</li>
         ))}
       </ul>
