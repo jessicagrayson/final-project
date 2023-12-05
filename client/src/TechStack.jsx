557b52b0c6b39e1d69ec2fea7981600477b8cac0
jessicagrayson <grayjessica12@hotmail.com>
2023-12-05 08:18:29 +0000

    storing changes

--- client/src/TechStack.jsx
+++ client/src/TechStack.jsx
@@ -28,25 +28,27 @@ export default function TechStack() {
   ];
 
   return (
-    <div className="flex flex-col items-center w-2/3 p-4 m-6 space-y-3 text-lg rounded-sm bg-slate-100">
-      <p>
+    <div className="flex flex-col items-center w-2/3 p-4 m-6 mx-auto space-y-3 text-lg rounded-sm bg-slate-100">
+      <p className="px-6 mx-5">
         Gophr is a full-stack photo blogging web application built by Jessica
         Grayson with a little help from the following resources and
         technologies:
       </p>
-      <ul className="list-disc">
-        <h1 className="font-semibold">Tech Stack:</h1>
-        {technologies.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
-      <ul className="list-disc">
-        <h1 className="font-semibold">Resources:</h1>
+      <div className="flex flex-row py-4 space-x-5">
+        <ul className="list-disc">
+          <h1 className="mb-2 font-semibold">Tech Stack:</h1>
+          {technologies.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
+        </ul>
+        <ul className="list-disc">
+          <h1 className="mb-2 font-semibold">Resources:</h1>
 
-        {resources.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
+          {resources.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
