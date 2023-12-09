1dcfc8422c2a41bd52477e31d68157c42ec1119e
jessicagrayson <grayjessica12@hotmail.com>
2023-12-09 00:43:40 +0000

    styling added to homepage link on guest feed

--- client/src/GuestFeed.jsx
+++ client/src/GuestFeed.jsx
@@ -57,44 +57,46 @@ const demoData = [
 
 export default function GuestFeed() {
   return (
-    <div className="flex flex-row flex-wrap items-center justify-center gap-y-2 gap-x-3">
-      <GuestModal />
-      {demoData.map((entry) => (
-        <div
-          key={entry.id}
-          className="flex p-3 mb-2 space-x-4 rounded-md bg-slate-200 hover:-translate-y-0.5 transition ease-in-out delay-70">
-          <ImageField
-            src={entry.imageUrl}
-            className="w-40 h-40 my-1 rounded-md"
-          />
+    <div className="flex flex-col flex-wrap items-center justify-center ">
+      <div className="flex flex-row flex-wrap items-center justify-center gap-y-2 gap-x-3">
+        <GuestModal />
+        {demoData.map((entry) => (
+          <div
+            key={entry.id}
+            className="flex p-3 mb-2 space-x-4 rounded-md bg-slate-200 hover:-translate-y-0.5 transition ease-in-out delay-70">
+            <ImageField
+              src={entry.imageUrl}
+              className="w-40 h-40 my-1 rounded-md"
+            />
 
-          <div className="flex flex-col">
-            <Input
-              className="text-sm font-semibold bg-slate-200"
-              placeholder="Location"
-              value={entry.location}
-              onChange={() => {}}
-            />
-            <Input
-              className="text-sm font-semibold bg-slate-200"
-              placeholder="Location"
-              value={entry.travelDate}
-              onChange={() => {}}
-            />
+            <div className="flex flex-col">
+              <Input
+                className="text-sm font-semibold bg-slate-200"
+                placeholder="Location"
+                value={entry.location}
+                onChange={() => {}}
+              />
+              <Input
+                className="text-sm font-semibold bg-slate-200"
+                placeholder="Location"
+                value={entry.travelDate}
+                onChange={() => {}}
+              />
+            </div>
+            <div className="flex items-end">
+              <LinkComponent
+                placeholder={'Details'}
+                className="text-sm text-indigo-500 hover:underline hover:text-indigo-500"
+              />
+            </div>
           </div>
-          <div className="flex items-end">
-            <LinkComponent
-              placeholder={'Details'}
-              className="text-sm text-indigo-500 hover:underline hover:text-indigo-600"
-            />
-          </div>
-        </div>
-      ))}
+        ))}
+      </div>
       <div>
         <LinkComponent
           to="/"
           placeholder="Homepage"
-          className="text-lg text-indigo-500 hover:underline"
+          className="text-xl tracking-wide text-indigo-500 hover:underline"
         />
       </div>
     </div>
